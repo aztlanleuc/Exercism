a6735eb2512a5b7d6ea80f61d3c781cc0a91f3d5
aztlanleuc <dm.phone766@gmail.com>
2021-05-07 16:47:51 +1000

    solution to secret handshake problem

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -3,6 +3,51 @@
 // convenience to get you started writing code faster.
 //
 
-export const commands = () => {
-  throw new Error('Remove this statement and implement this function');
+export const commands = (input) => {
+  let output = [];
+
+  let inputBin = input.toString(2);
+
+  let inputArray;
+
+  if (!(input > 16)) {
+    inputArray = inputBin.split("").reverse();
+
+    if (inputArray[0] === "1") {
+      output.push("wink");
+    }
+
+    if (inputArray[1] === "1") {
+      output.push("double blink");
+    }
+
+    if (inputArray[2] === "1") {
+      output.push("close your eyes");
+    }
+
+    if (inputArray[3] === "1") {
+      output.push("jump");
+    }
+  } else {
+    inputArray = inputBin.split("");
+    inputArray.shift();
+
+    if (inputArray[0] === "1") {
+      output.push("jump");
+    }
+
+    if (inputArray[1] === "1") {
+      output.push("close your eyes");
+    }
+
+    if (inputArray[2] === "1") {
+      output.push("double blink");
+    }
+
+    if (inputArray[3] === "1") {
+      output.push("wink");
+    }
+  }
+
+  return output;
 };
