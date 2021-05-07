49aca50e9874d7dc6de98ef8d45ed68debb877d2
aztlanleuc <dm.phone766@gmail.com>
2021-05-07 10:50:27 +1000

    working version and cleaned up console logs

--- javascript/word-search/word-search.js
+++ javascript/word-search/word-search.js
@@ -18,19 +18,14 @@ class WordSearch {
   find(wordArray) {
     // recursive version
 
-    console.log("new test");
-
     for (let word of wordArray) {
       for (let row in this.grid) {
         row = Number(row);
         for (let column in this.grid[row]) {
           column = Number(column);
           if (this.grid[row][column] === word[0]) {
-            console.log("we found the first letter");
             let endLocation = this.searchRecursively(word, 1, row, column);
-            console.log("endLocation:", endLocation); // DEBUG
-            // console.log("row, typeof row:", row, typeof row); // DEBUG
-            // console.log("column, typeof column:", column, typeof column); // DEBUG
+
             if (endLocation) {
               this.matches[word] = {
                 start: [row + 1, column + 1],
@@ -42,32 +37,7 @@ class WordSearch {
       }
     }
 
-    // console.log(this.matches);
     return this.matches;
-  }
-
-  searchForwards(word, line, linenum) {
-    linenum = Number(linenum) + 1;
-    let start = line.search(word) + 1;
-    let end = start + word.length - 1;
-    if (start !== 0) {
-      this.matches[word] = { start: [linenum, start], end: [linenum, end] };
-    }
-  }
-
-  searchBackwards(word, line, linenum) {
-    linenum = Number(linenum) + 1;
-
-    // reverse the word so that we can find backwards versions
-    let temp = word.split("");
-    temp.reverse();
-    let wordReverse = temp.join("");
-
-    let end = line.search(wordReverse) + 1;
-    let start = end + wordReverse.length - 1;
-    if (end !== 0) {
-      this.matches[word] = { start: [linenum, start], end: [linenum, end] };
-    }
   }
 
   transformInputGrid(input) {
@@ -80,10 +50,6 @@ class WordSearch {
   }
 
   searchRecursively(string, index, rownum, columnnum, dir) {
-    console.log("function called:", rownum, columnnum); // DEBUG
-
-    console.log("dir:", dir); // DEBUG
-
     let i;
     let j;
 
@@ -94,35 +60,37 @@ class WordSearch {
 
     for (i = top; i <= bottom; i++) {
       for (j = left; j <= right; j++) {
-        console.log("i, j:", i, j); // DEBUG
         if (dir) {
           if (!(i === dir[0] && j === dir[1])) {
             continue;
           }
         }
 
-        try {
-          if (i === middle && j === middle) {
+        if (i === middle && j === middle) {
+          continue;
+        }
+
+        // if we've hit the edge of the grid, move on to the next cell
+        if (
+          this.grid[rownum + i] === undefined ||
+          this.grid[rownum + i][columnnum + j] === undefined
+        ) {
+          continue;
+        }
+
+        if (this.grid[rownum + i][columnnum + j] === string[index]) {
+          let result = this.searchRecursively(
+            string,
+            index + 1,
+            rownum + i,
+            columnnum + j,
+            [i, j]
+          );
+          if (!result) {
             continue;
           }
 
-          // if we've hit the edge of the grid, return that we haven't found the string
-          if (this.grid[i] === undefined || this.grid[i][j] === undefined) {
-            continue;
-          }
-
-          if (this.grid[rownum + i][columnnum + j] === string[index]) {
-            // console.log("matched next letter");
-            return this.searchRecursively(
-              string,
-              index + 1,
-              rownum + i,
-              columnnum + j,
-              [i, j]
-            );
-          }
-        } catch (err) {
-          // console.log("err:", err);
+          return result;
         }
       }
     }
