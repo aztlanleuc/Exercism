85e2943ec1ecf423c007cd447dcd14f45dba87bc
aztlanleuc <dm.phone766@gmail.com>
2021-07-23 10:08:24 +1000

    Now passes all but final test

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -5,3 +5,50 @@ Write your code for the 'Robot Name' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/robot-name` directory.
 =end
+
+module Robot
+    def self.forget
+        # do nothing
+        puts "\n--------------------"
+    end
+
+    class Robots 
+        @@taken_names = []
+
+        attr_reader :name
+
+        def initialize
+            @name = generate_name
+        end
+
+        def generate_name
+            letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
+            name = ""
+            loop do
+                name = letters[rand(26)] + letters[rand(26)] + rand(100..1000).to_s
+                # puts name
+                break if !@@taken_names.include? name
+            end
+
+            @@taken_names.push name
+
+            name
+        end
+
+        def reset
+            @@taken_names.delete(@name)
+
+            @name = generate_name
+        end
+    end
+
+    def self.new
+        return Robots.new
+    end
+end
+
+
+robot = Robot::Robots.new
+
+robot.generate_names
