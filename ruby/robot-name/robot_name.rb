98bfaf242f550fea3847f97efc19e41f99e47ff5
aztlanleuc <dm.phone766@gmail.com>
2021-07-23 11:44:23 +1000

    Code cleanup

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -9,42 +9,22 @@ To get started with TDD, see the `README.md` file in your
 module Robot
     def self.forget
         Robots::regenerate_names
-        puts "\n--------------------"
     end
 
-    class Robots 
-        letters = ("A".."Z").to_a.repeated_permutation(2).to_a
-        letter_combos = letters.map do |element|
-            element.join("")
-        end
-
-        numbers = (0..9).to_a.repeated_permutation(3).to_a
-        number_combos = numbers.map do |element|
-            element.join("")
-        end
-
-        valid_name_arrays = letter_combos.product(number_combos)
-
-        @@available_names = valid_name_arrays.map { |elem| elem.join("") }
-
+    class Robots
         attr_reader :name
 
         def initialize
+            regenerate_names unless @@available_names # generate the available names if they don't already exist
             @name = generate_name
         end
 
         def generate_name
             index = rand(@@available_names.length)
 
-            # puts index
-
-            name = @@available_names.fetch(index, nil)
+            name = @@available_names[index]
 
             @@available_names.delete_at(index)
-
-            puts "there are #{@@available_names.length} names remaining"
-
-            # puts name
 
             name
         end
@@ -57,20 +37,14 @@ module Robot
 
         def self.regenerate_names
             letters = ("A".."Z").to_a.repeated_permutation(2).to_a
-            letter_combos = letters.map do |element|
-                element.join("")
-            end
+            letter_combos = letters.map { |element| element.join("") }
 
             numbers = (0..9).to_a.repeated_permutation(3).to_a
-            number_combos = numbers.map do |element|
-                element.join("")
-            end
+            number_combos = numbers.map { |element| element.join("") }
 
             valid_name_arrays = letter_combos.product(number_combos)
 
             valid_names = valid_name_arrays.map { |elem| elem.join("") }
-
-            # puts "the number of valid names is #{valid_names.length}"
 
             @@available_names = valid_names
         end
