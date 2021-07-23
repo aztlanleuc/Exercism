b200832bce3d756bc34a869431a2fb18abbd9e8c
aztlanleuc <dm.phone766@gmail.com>
2021-07-23 16:46:53 +1000

    Code cleaning

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,48 +1,45 @@
 module RunLengthEncoding
     def self.encode(input)
-=begin
-            take the first letter
-            continue until you find a letter that is not the first letter and store that location
-            work out how many letters matched and add to the encoding string
-            repeat
-=end
-
         encoded_version = ""
 
-        if input.length == 0
-            return ""
-        end
-
         for i in 0..input.length - 1 
-            # puts "----------"
-
-            # puts "#{input[i]} => #{input[i + 1]}"
-
             current_letter ||= input[i]
             sequence_start ||= i
-            # puts "we are counting #{current_letter}'s and they started at #{sequence_start}"
 
             if input[i] != input[i + 1]
-                # puts "they don't match"
-
                 count = i + 1 - sequence_start
+
                 unless count == 1
                     encoded_version << "#{count}#{current_letter}"
                 else
                     encoded_version << current_letter
                 end
-                
-
 
                 current_letter = nil
                 sequence_start = nil
             end
         end
 
-        # puts encoded_version
-
         return encoded_version
     end
-end
+    def self.decode(input)
+        pattern = /\d*[A-z ]/
+        
+        match_data = input.scan(pattern)
 
-RunLengthEncoding.encode("XYZ")
+        output_string = ""
+
+        match_data.each do |elem|
+            letter = elem[-1]
+            number = elem[0...-1].to_i
+
+            unless number == 0
+                number.times { output_string << letter }
+            else 
+                output_string << letter
+            end
+        end
+
+        return output_string
+    end
+end
