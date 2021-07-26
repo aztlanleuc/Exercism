43cf1ddf67c081f233dcd53cf90158ff574beb72
aztlanleuc <dm.phone766@gmail.com>
2021-07-26 14:36:16 +1000

    Now handles case insensitivity and line number flags

--- ruby/grep/grep.rb
+++ ruby/grep/grep.rb
@@ -19,26 +19,58 @@ module Grep
 
         files.each do |text| 
             file = File.open(text)
-            output << find_matches(pattern, file)
+            file.readlines.each_with_index do |line, index|
+                print "\n#{index + 1}:"
+
+                case_insensitive = (flags.include? "-i") ? true : false
+
+                match = find_matches(pattern, line, case_insensitive)
+
+                f_name = (flags.include? "-l") ? text : false
+                l_num = (flags.include? "-n") ? index + 1 : false
+
+                output << edit_output(match, f_name, l_num)
+            end
         end
 
         return output.rstrip
     end
 
-    def self.find_matches(pattern, text)
+    def self.find_matches(pattern, line, case_insensitive) # this function will also need to handle inversions i think
         output = ""
 
-        text.readlines.each_with_index do |line, index|
-            puts "#{index}: #{line}"
+        line_original = line.clone
 
-            if line.include? pattern
-                puts "match found"
-                output << line
-            end
+        if case_insensitive
+            pattern.downcase!
+            line.downcase!
         end
 
-        puts output
+        puts "after case: #{line_original}"
+        
+        if line.include? pattern
+            puts "match found"
+            output << line_original
+        end
 
         return output
+    end
+
+    def self.edit_output(matching_line, file_name, line_num)
+        # puts "file name: #{file_name}, line num: #{line_num}"
+
+        unless matching_line == ""
+            if file_name
+                # puts "file names only"
+                return file_name
+            elsif line_num
+                # puts "include line numbers"
+                return "#{line_num}:#{matching_line}"
+            else
+                # puts "just the line"
+                return matching_line
+            end
+        else return ""
+        end
     end
 end
--- ruby/grep/grep_test.rb
+++ ruby/grep/grep_test.rb
@@ -58,7 +58,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_print_line_numbers_flag
-    skip
+    # skip
     pattern = "Forbidden"
     flags = ["-n"]
     files = ["paradise-lost.txt"]
@@ -70,7 +70,7 @@ class GrepTest < Minitest::Test
   end
 
   def test_one_file_one_match_case_insensitive_flag
-    skip
+    # skip
     pattern = "FORBIDDEN"
     flags = ["-i"]
     files = ["paradise-lost.txt"]
